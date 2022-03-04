59aae888b7378066d7528ca5366d3db2f002bacb
Adrien Olivon <adrien.olivon.dev@gmail.com>
2022-03-05 00:57:21 +0100

    Index with pretty search !

---
+++ vue/src/partials/search/App.vue
@@ -0,0 +1,131 @@
+<template>
+  <div>
+    <div class="input-group mb-3 input-group-lg">
+      <span class="input-group-text">GO /</span>
+      <input
+        v-model="search"
+        type="text"
+        placeholder="Search"
+        @keydown="reset_index_if_letter"
+        class="form-control"
+      />
+    </div>
+    <div>
+      <ol class="list-group">
+        <li
+          v-for="(shortcut, i) in fuzzed_or_all"
+          :key="i"
+          :class="{ active: i == selected_index }"
+          class="list-group-item d-flex justify-content-between align-items-start"
+        >
+          <div class="ms-2 me-auto">
+            <span class="fw-bold">
+              {{ shortcut.shortcut }}
+            </span>
+            {{ shortcut.url }}
+          </div>
+        </li>
+      </ol>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useVueFuse } from "vue-fuse";
+
+interface Window {
+  shortcuts: Shortcut[];
+}
+
+interface Shortcut {
+  shortcut: string;
+  url: string;
+}
+
+const CONTROL_KEYS = ["ArrowUp", "ArrowDown", "Enter", "Tab"];
+const SHORTCUTS = (window as unknown as Window).shortcuts;
+
+let key_press: (e: KeyboardEvent) => void;
+
+export default defineComponent({
+  name: "App",
+  setup() {
+    const { search, results, noResults } = useVueFuse(SHORTCUTS, {
+      keys: [
+        { name: "shortcut", weight: 2 },
+        { name: "url", weight: 1 },
+      ],
+    });
+
+    return {
+      search,
+      shortcuts_fuzzed: results,
+      noResults,
+    };
+  },
+  computed: {
+    fuzzed_or_all(): Shortcut[] {
+      return this.shortcuts_fuzzed.length
+        ? (this.shortcuts_fuzzed as unknown as Shortcut[])
+        : this.shortcuts;
+    },
+  },
+  data() {
+    return { selected_index: -1, shortcuts: SHORTCUTS };
+  },
+  created() {
+    key_press = (e: KeyboardEvent) => {
+      let key = e.key;
+      console.log(key);
+      if (CONTROL_KEYS.includes(key)) {
+        e.preventDefault();
+
+        if (key === "ArrowUp" && this.selected_index >= 0) {
+          this.selected_index -= 1;
+        }
+        if (
+          key === "ArrowDown" &&
+          this.selected_index + 1 < this.fuzzed_or_all.length
+        ) {
+          this.selected_index += 1;
+        }
+
+        if (key === "Tab") {
+          this.take_selected(this.selected_index < 0 ? 0 : this.selected_index);
+        }
+        if (key === "Enter") {
+          this.go_selected(this.selected_index);
+        }
+      }
+    };
+    window.addEventListener("keydown", key_press);
+  },
+  unmounted() {
+    window.removeEventListener("keydown", key_press);
+  },
+  methods: {
+    reset_index_if_letter({ key }: KeyboardEvent) {
+      if (key.length === 1) this.selected_index = -1;
+    },
+    take_selected(selected_index: number) {
+      this.search = this.fuzzed_or_all[selected_index].shortcut;
+      this.selected_index = -1;
+    },
+    go_selected(selected_index: number) {
+      this.fuzzed_or_all[selected_index];
+      window.location.href = "/" + this.fuzzed_or_all[selected_index].shortcut;
+    },
+  },
+});
+</script>
+
+<style>
+ul {
+  list-style: none;
+}
+
+.selected::before {
+  content: "🚀";
+}
+</style>
