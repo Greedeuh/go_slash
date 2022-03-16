1b43d9dedc614e5abdc5c0f8d16fc66d5c2a4429
Adrien Olivon <adrien.olivon.dev@gmail.com>
2022-03-16 19:11:59 +0100

    Add hover icons on shortcut link in admin mode

--- web/vue/src/partials/search/ShortcutList.vue
+++ web/vue/src/partials/search/ShortcutList.vue
@@ -5,16 +5,19 @@
       :href="administer ? shortcut.shortcut + '?no_redirect' : shortcut.url"
       :key="i"
       role="listitem"
-      :class="{ active: i == selected_index }"
+      :class="{ active: i == selected_index, 'no-redirect': administer }"
       class="list-group-item d-flex justify-content-between align-items-start"
       @click="click_shortcut_index(i)"
     >
-      <div class="ms-2 me-auto">
+      <div class="ms-2 me-auto content">
         <span class="fw-bold">
           {{ shortcut.shortcut }}
         </span>
         {{ shortcut.url }}
         <span v-if="shortcut.new" class="badge bg-success">NEW</span>
+        <span v-if="administer" class="edit-icon"
+          ><i class="icon-eye-open"></i
+        ></span>
       </div>
       <div
         v-if="administer"
@@ -56,8 +59,16 @@ export default defineComponent({
 });
 </script>
 
-<style>
+<style scoped>
 .list-group {
   justify-content: space-between;
+}
+
+.edit-icon {
+  display: none;
+}
+
+.no-redirect:hover .edit-icon {
+  display: inline;
 }
 </style>
