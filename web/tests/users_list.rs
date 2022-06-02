4f7507a58eca76ac3ceebb05925657ed2abd9a6a
Adrien Olivon <adrien.olivon.dev@gmail.com>
2022-06-02 21:37:48 +0200

    Rework user list tests

--- web/tests/users_list.rs
+++ web/tests/users_list.rs
@@ -13,7 +13,7 @@ use go_web::guards::SESSION_COOKIE;
 use utils::*;
 
 #[async_test]
-async fn layout_with_users_link_if_feature_login() {
+async fn link_are_shown_on_other_pages() {
     in_browser(
         "some_session_id: some_mail@mail.com",
         |driver: &WebDriver, con: Mutex<PgConnection>, port: u16| {
@@ -58,7 +58,7 @@ async fn layout_with_users_link_if_feature_login() {
 }
 
 #[async_test]
-async fn list_users() {
+async fn as_admin_i_can_see_the_list() {
     in_browser(
         "some_session_id: some_mail@mail.com",
         |driver: &WebDriver, con: Mutex<PgConnection>, port: u16| {
@@ -120,8 +120,69 @@ async fn list_users() {
                     );
                 }
 
-                switchs[0].click().await?;
-                assert_eq!(switchs[0].get_property("checked").await?.unwrap(), "true");
+                Ok(())
+            }
+            .boxed()
+        },
+    )
+    .await;
+}
+
+#[async_test]
+async fn as_admin_i_can_change_users_capabilities() {
+    in_browser(
+        "some_session_id: some_mail@mail.com",
+        |driver: &WebDriver, con: Mutex<PgConnection>, port: u16| {
+            async move {
+                let con = con.lock().await;
+                user(
+                    "some_mail@mail.com",
+                    "pwd",
+                    &[],
+                    &[Capability::UsersAdmin],
+                    &con,
+                );
+                user(
+                    "another_mail@mail.com",
+                    "pwd",
+                    &[],
+                    &[Capability::TeamsRead],
+                    &con,
+                );
+
+                driver
+                    .add_cookie(Cookie::new(SESSION_COOKIE, json!("some_session_id")))
+                    .await?;
+
+                driver
+                    .get(format!("http://localhost:{}/go/users", port))
+                    .await?;
+
+                assert_users(driver, vec!["another_mail@mail.com", "some_mail@mail.com"]).await;
+
+                let user = driver
+                    .find_elements(By::Css("[role='listitem']"))
+                    .await?
+                    .first()
+                    .unwrap()
+                    .clone();
+
+                user.click().await?;
+
+                let switchs = user.find_elements(By::Css("[role='switch']")).await?;
+                let switch = switchs.first().unwrap();
+                let switchs_label = user.find_elements(By::Tag("label")).await?;
+                let switch_label = switchs_label.first().unwrap();
+
+                switch_label.wait_until().displayed().await?;
+                assert_eq!(
+                    switch_label.text().await.unwrap(),
+                    Capability::Features.to_string()
+                );
+                assert_eq!(switch.get_property("checked").await?.unwrap(), "false");
+
+                switch.click().await?;
+                assert_eq!(switch.get_property("checked").await?.unwrap(), "true");
 
                 driver
                     .get(format!("http://localhost:{}/go/users", port))
@@ -144,14 +205,6 @@ async fn list_users() {
                 switch.click().await?;
                 assert_eq!(switch.get_property("checked").await?.unwrap(), "false");
 
-                driver
-                    .get(format!("http://localhost:{}/go/users", port))
-                    .await?;
-
-                let switchs = driver.find_elements(By::Css("[role='switch']")).await?;
-                let switch = &switchs[0];
-                assert_eq!(switch.get_property("checked").await?.unwrap(), "false");
-
                 Ok(())
             }
             .boxed()
@@ -170,87 +223,116 @@ async fn assert_users(driver: &WebDriver, expected_users: Vec<&str>) {
     }
 }
 
-#[test]
-fn put_user_capability_user_with_capability() {
-    let (client, conn) = launch_with("some_session_id: some_mail@mail.com");
-    user("some_mail@mail.com", "pwd", &[], &[], &conn);
+mod controller {
+    use super::*;
 
-    let response = client
-        .put("/go/users/some_mail@mail.com/capabilities/Features")
-        .dispatch();
+    #[test]
+    fn with_unknow_user_is_not_unauthorized() {
+        let (client, conn) = launch_with("some_session_id: some_mail@mail.com");
+        user("some_mail@mail.com", "pwd", &[], &[], &conn);
 
-    assert_eq!(response.status(), Status::Unauthorized);
+        let response = client
+            .put("/go/users/some_mail@mail.com/capabilities/Features")
+            .dispatch();
 
-    let response = client
-        .put("/go/users/some_mail@mail.com/capabilities/Features")
-        .cookie(http::Cookie::new(SESSION_COOKIE, "some_session_id"))
-        .dispatch();
+        assert_eq!(response.status(), Status::Unauthorized);
 
-    assert_eq!(response.status(), Status::Unauthorized);
-}
+        let response = client
+            .put("/go/users/some_mail@mail.com/capabilities/Features")
+            .cookie(http::Cookie::new(SESSION_COOKIE, "some_session_id"))
+            .dispatch();
 
-#[test]
-fn put_user_capability() {
-    let (client, conn) = launch_with("some_session_id: some_mail@mail.com");
-    user(
-        "some_mail@mail.com",
-        "pwd",
-        &[],
-        &[Capability::UsersAdmin],
-        &conn,
-    );
+        assert_eq!(response.status(), Status::Unauthorized);
+    }
 
-    let response = client
-        .put("/go/users/some_mail@mail.com/capabilities/Features")
-        .cookie(http::Cookie::new(SESSION_COOKIE, "some_session_id"))
-        .dispatch();
+    mod put {
+        use super::*;
+        #[test]
+        fn as_unknown_user_is_not_unauthorized() {
+            let (client, conn) = launch_with("some_session_id: some_mail@mail.com");
+            user("some_mail@mail.com", "pwd", &[], &[], &conn);
 
-    assert_eq!(response.status(), Status::Ok);
+            let response = client
+                .put("/go/users/some_mail@mail.com/capabilities/Features")
+                .dispatch();
 
-    let user = get_user("some_mail@mail.com", &conn).unwrap();
-    assert_eq!(
-        user.capabilities,
-        &[Capability::UsersAdmin, Capability::Features]
-    )
-}
+            assert_eq!(response.status(), Status::Unauthorized);
 
-#[test]
-fn delete_user_capability_user_with_capability() {
-    let (client, conn) = launch_with("some_session_id: some_mail@mail.com");
-    user("some_mail@mail.com", "pwd", &[], &[], &conn);
+            let response = client
+                .put("/go/users/some_mail@mail.com/capabilities/Features")
+                .cookie(http::Cookie::new(SESSION_COOKIE, "some_session_id"))
+                .dispatch();
 
-    let response = client
-        .delete("/go/users/some_mail@mail.com/capabilities/Features")
-        .dispatch();
+            assert_eq!(response.status(), Status::Unauthorized);
+        }
 
-    assert_eq!(response.status(), Status::Unauthorized);
+        #[test]
+        fn as_user() {
+            let (client, conn) = launch_with("some_session_id: some_mail@mail.com");
+            user(
+                "some_mail@mail.com",
+                "pwd",
+                &[],
+                &[Capability::UsersAdmin],
+                &conn,
+            );
 
-    let response = client
-        .delete("/go/users/some_mail@mail.com/capabilities/Features")
-        .cookie(http::Cookie::new(SESSION_COOKIE, "some_session_id"))
-        .dispatch();
+            let response = client
+                .put("/go/users/some_mail@mail.com/capabilities/Features")
+                .cookie(http::Cookie::new(SESSION_COOKIE, "some_session_id"))
+                .dispatch();
 
-    assert_eq!(response.status(), Status::Unauthorized);
-}
+            assert_eq!(response.status(), Status::Ok);
 
-#[test]
-fn delete_user_capability() {
-    let (client, conn) = launch_with("some_session_id: some_mail@mail.com");
-    user(
-        "some_mail@mail.com",
-        "pwd",
-        &[],
-        &[Capability::UsersAdmin, Capability::Features],
-        &conn,
-    );
+            let user = get_user("some_mail@mail.com", &conn).unwrap();
+            assert_eq!(
+                user.capabilities,
+                &[Capability::UsersAdmin, Capability::Features]
+            )
+        }
+    }
 
-    let response = client
-        .delete("/go/users/some_mail@mail.com/capabilities/Features")
-        .cookie(http::Cookie::new(SESSION_COOKIE, "some_session_id"))
-        .dispatch();
+    mod delete {
+        use super::*;
+        #[test]
+        fn as_unknown_user_is_not_unauthorized() {
+            let (client, conn) = launch_with("some_session_id: some_mail@mail.com");
+            user("some_mail@mail.com", "pwd", &[], &[], &conn);
 
-    assert_eq!(response.status(), Status::Ok);
+            let response = client
+                .delete("/go/users/some_mail@mail.com/capabilities/Features")
+                .dispatch();
 
-    let user = get_user("some_mail@mail.com", &conn).unwrap();
-    assert_eq!(user.capabilities, &[Capability::UsersAdmin,])
+            assert_eq!(response.status(), Status::Unauthorized);
+
+            let response = client
+                .delete("/go/users/some_mail@mail.com/capabilities/Features")
+                .cookie(http::Cookie::new(SESSION_COOKIE, "some_session_id"))
+                .dispatch();
+
+            assert_eq!(response.status(), Status::Unauthorized);
+        }
+
+        #[test]
+        fn as_user() {
+            let (client, conn) = launch_with("some_session_id: some_mail@mail.com");
+            user(
+                "some_mail@mail.com",
+                "pwd",
+                &[],
+                &[Capability::UsersAdmin, Capability::Features],
+                &conn,
+            );
+
+            let response = client
+                .delete("/go/users/some_mail@mail.com/capabilities/Features")
+                .cookie(http::Cookie::new(SESSION_COOKIE, "some_session_id"))
+                .dispatch();
+
+            assert_eq!(response.status(), Status::Ok);
+
+            let user = get_user("some_mail@mail.com", &conn).unwrap();
+            assert_eq!(user.capabilities, &[Capability::UsersAdmin,])
+        }
+    }
 }
